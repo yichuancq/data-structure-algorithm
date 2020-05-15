c498a0c69e699d7300ba6b7fa393925b0731d781
yichuancq <1012027293@qq.com>
2020-05-15 20:40:50 +0800

    fix package

--- src/main/java/treenode/TreeNode.java
+++ src/main/java/treenode/TreeNode.java
@@ -1,18 +1,11 @@
 package treenode;
 
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.io.Serializable;
 
 /**
  * @author yichuan
  */
-@Getter
-@Setter
-@ToString
+
 /**
  * 树节点定义
  */
@@ -30,5 +23,29 @@ public class TreeNode<T> implements Serializable {
     public TreeNode(T value) {
         this.value = value;
 
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public TreeNode<T> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode<T> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode<T> getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode<T> rightChild) {
+        this.rightChild = rightChild;
     }
 }
